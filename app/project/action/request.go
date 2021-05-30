6ea93db7eb2224ecda4bb73b4d69c0f2aa44eb52
Kyle Unverferth <kyle@kyleu.com>
2021-05-30 17:41:57 -0600

    UI fixes

--- app/project/action/request.go
+++ app/project/action/request.go
@@ -15,8 +15,15 @@ func NewRequest(t string, a string, params ...interface{}) *Request {
 	return &Request{Type: t, Action: a, Params: util.ValueMapFor(params...)}
 }
 
-func Qualify(req *Request, acts Actions) ([]interface{}, error) {
-	var ret []interface{}
+type Result struct {
+	Action []string `json:"act"`
+	Path   []string `json:"path"`
+}
+
+type Results []*Result
+
+func Qualify(req *Request, acts Actions) (Results, error) {
+	var ret Results
 	for _, act := range acts {
 		childResult, err := qualifyAct(req, act)
 		if err != nil {
@@ -27,40 +34,46 @@ func Qualify(req *Request, acts Actions) ([]interface{}, error) {
 	return ret, nil
 }
 
-func qualifyAct(req *Request, act *Action) ([]interface{}, error) {
-	var ret []interface{}
+func qualifyAct(req *Request, act *Action) (Results, error) {
+	var ret Results
 
-	switch act.Type {
-	case TypeFolder:
-	case TypeStatic:
-	case TypeSeparator:
-
-	case TypeAll:
-		if req.Type == TypeModel.Key {
-			ret = append(ret, "All!!!")
+	switch req.Type {
+	case TypeModel.Key:
+		modelPath, err := req.Params.GetString(TypeModel.Key, false)
+		if err != nil {
+			return nil, err
 		}
-	case TypeSource:
-		if req.Type == TypeModel.Key {
-			ret = append(ret, "Source!!!")
+		srcKey, err := req.Params.GetString(TypeSource.Key, false)
+		if err != nil {
+			return nil, err
 		}
-	case TypePackage:
-		if req.Type == TypeModel.Key {
-			ret = append(ret, "Package!!!")
+		ret, err = qualifyModel(req, act, srcKey, modelPath)
+		if err != nil {
+			return nil, err
 		}
-	case TypeModel:
-		if req.Type == TypeModel.Key {
-			ret = append(ret, "Model!!!")
-		}
-	case TypeActivity:
-		ret = append(ret, "Activity!!!")
-
 	default:
-		return nil, errors.New("unhandled action type [" + act.Type.Key + "] in qualify attempt")
+		return nil, errors.New("unhandled qualify type [" + req.Type + "]")
 	}
+
 	kids, err := Qualify(req, act.Children)
 	if err != nil {
 		return nil, err
 	}
 	ret = append(ret, kids...)
 	return ret, nil
+}
+
+func qualifyModel(req *Request, act *Action, srcKey string, modelPath string) (Results, error) {
+	p := util.SplitAndTrim(modelPath, "/")
+	switch act.Type {
+	case TypeAll:
+		return Results{{Action: act.Path(), Path: p}}, nil
+	case TypeSource:
+		return Results{{Action: act.Path(), Path: p}}, nil
+	case TypePackage:
+		return Results{{Action: act.Path(), Path: p}}, nil
+	case TypeModel:
+		return Results{{Action: act.Path(), Path: p}}, nil
+	}
+	return nil, nil
 }
