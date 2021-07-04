eddc3c3bfc26bc2884b652cce9504d6a18e35fd8
Kyle Unverferth <kyle@kyleu.com>
2021-07-04 15:27:23 -0600

    Asset pipeline improvements

--- bin/release.sh
+++ bin/release.sh
@@ -7,7 +7,5 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $dir/..
 
 ./bin/asset-embed.sh
-git update-index --assume-unchanged app/assets/assets.go
 goreleaser release --rm-dist
 ./bin/asset-reset.sh
-git update-index --assume-unchanged app/assets/assets.go
