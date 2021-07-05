7a35caeae1da673b42fd9203ffc30010cdb22ba5
Kyle Unverferth <kyle@kyleu.com>
2021-07-05 15:48:36 -0600

    Bootstrapping an app state for site

--- app/cmd/site.go
+++ app/cmd/site.go
@@ -1,16 +1,20 @@
 package cmd
 
 import (
+	"github.com/kyleu/admini/app"
 	"github.com/kyleu/admini/app/controller"
+	"github.com/kyleu/admini/app/filesystem"
 	"go.uber.org/zap"
 )
 
 func startSite(flags *Flags, logger *zap.SugaredLogger) (*zap.SugaredLogger, error) {
 	r := controller.SiteRoutes()
-	// f := filesystem.NewFileSystem(flags.ConfigDir, logger)
-
-	controller.SetState(nil, logger)
-
+	f := filesystem.NewFileSystem(flags.ConfigDir, logger)
+	st, err := app.NewState(flags.Debug, AppBuildInfo, r, f, nil, logger)
+	if err != nil {
+		return logger, err
+	}
+	controller.SetState(st, logger)
 	return webserver(flags.Addr(), logger, r)
 }
 
