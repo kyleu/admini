f319946b4783b838a48a34e795ebea4cdddfa76b
Kyle Unverferth <kyle@kyleu.com>
2021-07-05 09:16:42 -0600

    CI

--- bin/bootstrap.bat
+++ bin/bootstrap.bat
@@ -1,5 +1,4 @@
 go get -u github.com/cosmtrek/air
 go get -u github.com/pyros2097/go-embed
 go get -u github.com/valyala/quicktemplate
-go get -u golang.org/x/tools/cmd/goimports
 go get -u mvdan.cc/gofumpt
