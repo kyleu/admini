76ed379a9f3440bb5fc0715f0320350d46bcaab9
Kyle Unverferth <kyle@kyleu.com>
2021-07-05 09:54:58 -0600

    CI

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -9,5 +9,5 @@ cd $dir/..
 go get -u github.com/cosmtrek/air
 go get -u github.com/pyros2097/go-embed
 go get -u github.com/valyala/quicktemplate
-go get -u golang.org/x/tools/cmd/goimports
+go get -u github.com/valyala/quicktemplate/qtc
 go mod download
