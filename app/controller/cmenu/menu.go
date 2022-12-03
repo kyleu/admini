88d0802487400a2b766e285d4e2dda86cefe660e
Kyle Unverferth <kyle@kyleu.com>
2022-12-03 15:32:40 -0700

    Upstream changes

--- app/controller/cmenu/menu.go
+++ app/controller/cmenu/menu.go
@@ -8,7 +8,6 @@ import (
 	"admini.dev/admini/app/lib/filter"
 	"admini.dev/admini/app/lib/menu"
 	"admini.dev/admini/app/lib/sandbox"
-	"admini.dev/admini/app/lib/telemetry"
 	"admini.dev/admini/app/lib/user"
 	"admini.dev/admini/app/util"
 )
@@ -16,10 +15,6 @@ import (
 func MenuFor(
 	ctx context.Context, isAuthed bool, isAdmin bool, profile *user.Profile, params filter.ParamSet, as *app.State, logger util.Logger,
 ) (menu.Items, any, error) {
-	ctx, span, logger := telemetry.StartSpan(ctx, "menu:generate", logger)
-	defer span.Complete()
-	_ = logger
-
 	var ret menu.Items
 	var data any
 	// $PF_SECTION_START(routes_start)$
--- app/state.go
+++ app/state.go
@@ -4,6 +4,7 @@ package app
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	"admini.dev/admini/app/lib/auth"
@@ -12,6 +13,8 @@ import (
 	"admini.dev/admini/app/lib/theme"
 	"admini.dev/admini/app/util"
 )
+
+var once sync.Once
 
 type BuildInfo struct {
 	Version string `json:"version"`
@@ -39,11 +42,18 @@ type State struct {
 }
 
 func NewState(debug bool, bi *BuildInfo, f filesystem.FileLoader, enableTelemetry bool, port uint16, logger util.Logger) (*State, error) {
-	loc, err := time.LoadLocation("UTC")
-	if err != nil {
-		return nil, err
+	var loadLocationError error
+	once.Do(func() {
+		loc, err := time.LoadLocation("UTC")
+		if err != nil {
+			loadLocationError = err
+			return
+		}
+		time.Local = loc
+	})
+	if loadLocationError != nil {
+		return nil, loadLocationError
 	}
-	time.Local = loc
 
 	_ = telemetry.InitializeIfNeeded(enableTelemetry, bi.Version, logger)
 	as := auth.NewService("", port, logger)
