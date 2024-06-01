3ac3de9714cd3fd35677a01b00c320cfc808f5a3
Kyle Unverferth <kyle@kyleu.com>
2024-06-01 15:20:05 -0600

    SVG Improvements

--- app/controller/cutil/page.go
+++ app/controller/cutil/page.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
-	"slices"
 	"time"
 
 	"github.com/samber/lo"
@@ -85,7 +84,6 @@ func (p *PageState) AddIcon(keys ...string) {
 			p.Icons = append(p.Icons, k)
 		}
 	})
-	slices.Sort(p.Icons)
 }
 
 func (p *PageState) TitleString() string {
