be4f9a129547ba7aab10f9124146e484b80d382e
Kyle Unverferth <kyle@kyleu.com>
2024-06-16 17:17:46 -0600

    Menu generation telemetry

--- app/controller/cmenu/menu.go
+++ app/controller/cmenu/menu.go
@@ -8,6 +8,7 @@ import (
 	"admini.dev/admini/app/lib/filter"
 	"admini.dev/admini/app/lib/menu"
 	"admini.dev/admini/app/lib/sandbox"
+	"admini.dev/admini/app/lib/telemetry"
 	"admini.dev/admini/app/lib/user"
 	"admini.dev/admini/app/util"
 )
@@ -15,6 +16,8 @@ import (
 func MenuFor(
 	ctx context.Context, isAuthed bool, isAdmin bool, profile *user.Profile, params filter.ParamSet, as *app.State, logger util.Logger, //nolint:revive
 ) (menu.Items, any, error) {
+	ctx, sp, _ := telemetry.StartSpan(ctx, "menu", logger)
+	defer sp.Complete()
 	var ret menu.Items
 	var data any
 	// $PF_SECTION_START(menu)$
