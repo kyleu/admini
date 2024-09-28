67abcfb69d4dd1570a64d1f1cebab8ddf762cfe0
Kyle Unverferth <kyle@kyleu.com>
2024-09-28 10:28:39 -0600

    Utility method and script improvements, process and UI enhancements

--- bin/dev.sh
+++ bin/dev.sh
@@ -9,6 +9,10 @@ cd "$dir/.."
 # $PF_SECTION_START(keys)$
 # $PF_SECTION_END(keys)$
 
+if command -v title &> /dev/null; then
+  title "admini"
+fi
+
 [[ -f "$HOME/bin/oauth" ]] && . "$HOME/bin/oauth"
 export admini_encryption_key=TEMP_SECRET_KEY
 
